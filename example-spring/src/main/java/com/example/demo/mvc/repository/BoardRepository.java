9d227ac1a41da4b8d5b64f4e6b0247bc2c344cfa
jskim <kjsj20@naver.com>
2022-06-01 18:03:35 +0900

    add board controller, repository, service

---
+++ example-spring/src/main/java/com/example/demo/mvc/repository/BoardRepository.java
@@ -0,0 +1,24 @@
+package com.example.demo.mvc.repository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.mvc.domain.Board;
+
+/**
+ * 게시판 Repository
+ * @author 김재성
+ */
+@Repository
+public interface BoardRepository {
+	List<Board> getList();
+	
+	Board get(int boardSeq);
+	
+	void save(Board board);
+	
+	void update(Board board);
+	
+	void delete(int boardSeq);
+}
